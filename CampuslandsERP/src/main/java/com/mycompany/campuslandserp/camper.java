8fda76a043ac945fadd3f0a2ebac8b27ec3c8b63
eduar <chanaga250@gmail.com>
2024-07-23 07:05:36 -0500

    feat: :sparkles: class camper
    se creo la clase camper

---
+++ CampuslandsERP/src/main/java/com/mycompany/campuslandserp/camper.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.campuslandserp;
+
+/**
+ *
+ * @author camper
+ */
+public class camper {
+    private int identificacion;
+    private String nombre;
+    private String direccion;
+    private String acudiente;
+    private int celular;
+    private int fijo;
+    private String estado;
+    private String riesgo;
+    
+    // Getter para identificacion
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    // Setter para identificacion
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    // Getter para nombre
+    public String getNombre() {
+        return nombre;
+    }
+
+    // Setter para nombre
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Getter para direccion
+    public String getDireccion() {
+        return direccion;
+    }
+
+    // Setter para direccion
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    // Getter para acudiente
+    public String getAcudiente() {
+        return acudiente;
+    }
+
+    // Setter para acudiente
+    public void setAcudiente(String acudiente) {
+        this.acudiente = acudiente;
+    }
+
+    // Getter para celular
+    public int getCelular() {
+        return celular;
+    }
+
+    // Setter para celular
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    // Getter para fijo
+    public int getFijo() {
+        return fijo;
+    }
+
+    // Setter para fijo
+    public void setFijo(int fijo) {
+        this.fijo = fijo;
+    }
+
+    // Getter para estado
+    public String getEstado() {
+        return estado;
+    }
+
+    // Setter para estado
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    // Getter para riesgo
+    public String getRiesgo() {
+        return riesgo;
+    }
+
+    // Setter para riesgo
+    public void setRiesgo(String riesgo) {
+        this.riesgo = riesgo;
+    }
+};
